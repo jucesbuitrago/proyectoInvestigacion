bbf3ce3c1cb5aca84acf9064cd9338129ddfae65
Jucesbuitrago <cesarbuitrago78@gmail.com>
2024-10-28 22:46:14 -0500

    Create proyect

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css';
+
+export function App () {
+    return (
+        <article className='tw-followCard'>
+            <header className='tw-followCard-header'>
+                <img className='tw-followCard-avatar'
+                 src="https://unavatar.io/s0yvalen" alt="El avatar de s0yValen" />
+                <div className='tw-followCard-info'>
+                    <strong>Paula Vargas</strong>
+                    <span className='tw-followCard-infoUserName'>@s0yValen</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
