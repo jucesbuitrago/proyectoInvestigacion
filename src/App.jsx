437e3386ad1c9b4cd6160228d2392c4418ec2b0e
Jucesbuitrago <cesarbuitrago78@gmail.com>
2024-10-30 21:41:08 -0500

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css';
-import { TwitterFollowCard } from './TwitterFollowCard';
+// import { TwitterFollowCard } from './TwitterFollowCard';
+import Student from "./components/Student"
 
 export function App () {
-    const formatUserName = (userName) => `@{userName}`;
+    // const formatUserName = (userName) => `@{userName}`;
     
     return (
-        <section className='App'>
-            <TwitterFollowCard  formatUserName={formatUserName}  isFollowing userName="s0yValen" name="Valentina Vargas"/>
-            <TwitterFollowCard  formatUserName={formatUserName}  isFollowing={false} userName="jucesbuitrago" name="Jucesbuitrago"/>
-        </section>
+        // <section className='App'>
+        //     <TwitterFollowCard  formatUserName={formatUserName}  isFollowing userName="s0yValen" name="Valentina Vargas"/>
+        //     <TwitterFollowCard  formatUserName={formatUserName}  isFollowing={false} userName="jucesbuitrago" name="Jucesbuitrago"/>
+        // </section>
+        <Student/>
     )
 }
